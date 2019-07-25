2941a87eb9259fb1f4ef9e1655bbf2319e672329
Jacob <jacobashin@gmail.com>
2019-07-24 21:15:36 -0400

    first commit

---
+++ Assessment5a/Controllers/Home_Controller.cs
@@ -0,0 +1,39 @@
+﻿using Assessment5a.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assessment5a.Controllers
+{
+    public class Home_Controller : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        public IActionResult Welcome(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            if (!(login.Password == "opensesame"))
+            {
+                return RedirectToAction("WrongPassword");
+            }
+            ViewBag.Name = login.Name.ToUpper();
+            ViewBag.Length = login.Name.Length;
+            return View();
+        }
+   
+        public IActionResult WrongPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ Assessment5a/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment5a.Models
+{
+    public class Login
+    {
+        [ViewData]
+        [Required]
+        public string Name { get; set; }
+
+        [ViewData]
+        [Required]
+        public string Password { get; set; }
+    }
+}
